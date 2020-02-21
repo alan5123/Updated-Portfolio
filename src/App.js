c7a4cfcdaf9a7d40156661b4e0f4c9df40884e0e
alan5123 <alanparker5123@gmail.com>
2020-02-20 19:12:12 -0500

    added react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Header from "./components/Header"
+import NavBar from "./components/NavBar";
+function App() {
+  
+  document.body.style = 'background: STEELBLUE;';
+  return (
+    <div>
+     <NavBar/>
+     <Header/>
+     </div>
+
+  );
+}
+
+export default App;
